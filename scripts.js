2951299c0462de821a64a2d1f13c2ab206a4a625
huynh.nguyen <nghuynh.dev@gmail.com>
2022-09-21 17:01:15 +0700

    updating

--- scripts.js
+++ scripts.js
@@ -3,11 +3,26 @@ const panels = document.querySelectorAll('.card-img-overlay');
 
 buttons.forEach((btn, index) => {
   const panel = panels[index];
-
   btn.onclick = function () {
     panel.classList.toggle('card-default');
   }
 });
+
+let wrappers = document.querySelectorAll(".card-text");
+wrappers.forEach((wrapper, index) => {
+  document.addEventListener("DOMContentLoaded", () => {
+    let options = {
+      callback: function (isTruncated) { },
+      ellipsis: "\u2026 ",
+      height: null,
+      keep: null,
+      tolerance: 4,
+      truncate: "word",
+      watch: "window",
+    };
+    new Dotdotdot(wrapper, options);
+  });
+})
 
 
 
